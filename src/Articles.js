de693d9bca2aad4a03aa9bc46c62ebe9e992648f
tenshotanaka <tensho1211@icloud.com>
2017-12-05 03:46:21 +0900

    feat: add pagination

--- src/Articles.js
+++ src/Articles.js
@@ -5,6 +5,7 @@ import FaStar from 'react-icons/lib/fa/star'
 import immutable from 'immutable'
 
 import Api from './Api'
+import { Bootstrap3ishPaginator } from 'react-paginators'
 
 const api = new Api(`http://127.0.0.1:4000`);
 
@@ -24,6 +25,13 @@ class List extends Component {
         })
     }
 
+    handlePageClick(page) {
+        api.listArticles(page).then((result) => {
+            this.setState({articles: result.articles, current: page, last: result.links.last._page})
+        })
+    }
+
+
     handleFavorite(article, index) {
         article.isFavorite = article.isFavorite !== true;
         api.updateArticle(article.id, article).then((result) => {
@@ -34,6 +42,9 @@ class List extends Component {
     }
 
     render() {
+        const current = this.state && this.state.current || 1;
+        const last = this.state && parseInt(this.state.last, 10) || 1;
+
         return (
             <div>
                 <h2>Articles</h2>
@@ -46,6 +57,14 @@ class List extends Component {
                         </li>
                     ))}
                 </ul>
+                <div style={{padding: "30px", display: "flex", justifyContent: "center"}}>
+                    <Bootstrap3ishPaginator
+                        current = {current}
+                        last = {last}
+                        maxPageCount = {10}
+                        onClick = {this.handlePageClick.bind(this)}
+                    />
+                </div>
             </div>
         )
     }
